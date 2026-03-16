f3e169b1fbe1670477d8a25567af134f410d7507
Toon Verwerft <toonverwerft@gmail.com>
2026-03-16 11:49:10 +0100

    feat(analyzer): add `Psl\Type\nullish()` return type provider (#1391)

--- crates/analyzer/src/plugin/libraries/psl/type_/mod.rs
+++ crates/analyzer/src/plugin/libraries/psl/type_/mod.rs
@@ -1,9 +1,11 @@
 //! PSL type function providers.
 
 mod int_range;
+mod nullish;
 mod optional;
 mod shape;
 
 pub use int_range::IntRangeProvider;
+pub use nullish::NullishProvider;
 pub use optional::OptionalProvider;
 pub use shape::ShapeProvider;
