5e441ffce0fe9c7b72b2a32b216da9d254aab635
Oliver Stark <oliver.stark@picdrop.com>
2026-03-18 08:32:51 +0100

    feat(linter): add prefer-pre-increment rule (#1311)
    Co-authored-by: Seifeddine Gmati <29315886+azjezz@users.noreply.github.com>

--- crates/linter/src/settings.rs
+++ crates/linter/src/settings.rs
@@ -112,6 +112,7 @@ use crate::rule::PreferArrowFunctionConfig;
 use crate::rule::PreferEarlyContinueConfig;
 use crate::rule::PreferFirstClassCallableConfig;
 use crate::rule::PreferInterfaceConfig;
+use crate::rule::PreferPreIncrementConfig;
 use crate::rule::PreferStaticClosureConfig;
 use crate::rule::PreferViewArrayConfig;
 use crate::rule::PreferWhileLoopConfig;
@@ -316,6 +317,7 @@ pub struct RulesSettings {
     pub single_class_per_file: RuleSettings<SingleClassPerFileConfig>,
     pub readable_literal: RuleSettings<ReadableLiteralConfig>,
     pub yoda_conditions: RuleSettings<YodaConditionsConfig>,
+    pub prefer_pre_increment: RuleSettings<PreferPreIncrementConfig>,
     pub switch_continue_to_break: RuleSettings<SwitchContinueToBreakConfig>,
     pub use_wp_functions: RuleSettings<UseWpFunctionsConfig>,
     pub no_direct_db_query: RuleSettings<NoDirectDbQueryConfig>,
