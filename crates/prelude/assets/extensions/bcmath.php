c74e27eacafd1edcd5c8f764cfa3504d82ef1bc1
George Steel <george@net-glue.co.uk>
2026-04-28 11:42:26 +0100

    fix(prelude): improve `bcmath` stubs

--- crates/prelude/assets/extensions/bcmath.php
+++ crates/prelude/assets/extensions/bcmath.php
@@ -3,63 +3,117 @@
 namespace {
     /**
      * @pure
+     * @param numeric-string $num1
+     * @param numeric-string $num2
+     * @param non-negative-int|null $scale
+     * @return numeric-string
      */
     function bcadd(string $num1, string $num2, ?int $scale = null): string {}
 
     /**
      * @pure
+     * @param numeric-string $num1
+     * @param numeric-string $num2
+     * @param non-negative-int|null $scale
+     * @return numeric-string
      */
     function bcsub(string $num1, string $num2, ?int $scale = null): string {}
 
     /**
      * @pure
+     * @param numeric-string $num1
+     * @param numeric-string $num2
+     * @param non-negative-int|null $scale
+     * @return numeric-string
      */
     function bcmul(string $num1, string $num2, ?int $scale = null): string {}
 
     /**
      * @pure
-     *
+     * @param numeric-string $num1
+     * @param numeric-string $num2
+     * @param non-negative-int|null $scale
+     * @return numeric-string
      * @throws DivisionByZeroError
      */
     function bcdiv(string $num1, string $num2, ?int $scale = null): string {}
 
     /**
      * @pure
-     *
+     * @param numeric-string $num1
+     * @param numeric-string $num2
+     * @param non-negative-int|null $scale
+     * @return numeric-string
      * @throws DivisionByZeroError
      */
     function bcmod(string $num1, string $num2, ?int $scale = null): string {}
 
     /**
      * @pure
+     * @param numeric-string $num
+     * @param numeric-string $exponent
+     * @param non-negative-int|null $scale
+     * @return numeric-string
      */
     function bcpow(string $num, string $exponent, ?int $scale = null): string {}
 
     /**
      * @pure
+     * @param numeric-string $num
+     * @param non-negative-int|null $scale
+     * @return numeric-string
      */
     function bcsqrt(string $num, ?int $scale): string {}
 
+    /**
+     * @param non-negative-int|null $scale
+     * @return non-negative-int
+     */
     function bcscale(?int $scale = null): int {}
 
     /**
      * @pure
+     * @param numeric-string $num1
+     * @param numeric-string $num2
+     * @param non-negative-int|null $scale
+     * @return int<-1,1>
      */
     function bccomp(string $num1, string $num2, ?int $scale = null): int {}
 
     /**
      * @pure
+     * @param numeric-string $num
+     * @param numeric-string $exponent
+     * @param numeric-string $modulus
+     * @param non-negative-int|null $scale
+     * @return numeric-string
+     * @throws DivisionByZeroError
      */
     function bcpowmod(string $num, string $exponent, string $modulus, ?int $scale = null): string {}
 
+    /**
+     * @param numeric-string $num
+     * @return numeric-string
+     */
     function bcfloor(string $num): string {}
 
+    /**
+     * @param numeric-string $num
+     * @return numeric-string
+     */
     function bcceil(string $num): string {}
 
+    /**
+     * @param numeric-string $num
+     * @return numeric-string
+     */
     function bcround(string $num, int $precision = 0, RoundingMode $mode = RoundingMode::HalfAwayFromZero): string {}
 
     /**
-     * @return list<string>
+     * @param numeric-string $num1
+     * @param numeric-string $num2
+     * @param non-negative-int|null $scale
+     * @return array{0: numeric-string, 1: numeric-string}
      */
     function bcdivmod(string $num1, string $num2, ?int $scale = null): array {}
 }
@@ -67,31 +121,68 @@ namespace {
 namespace BcMath {
     final readonly class Number implements \Stringable
     {
+        /** @var numeric-string */
         public readonly string $value;
 
+        /** @var non-negative-int */
         public readonly int $scale;
 
+        /** @param numeric-string|int $num */
         public function __construct(string|int $num) {}
 
+        /**
+         * @param Number|numeric-string|int $num
+         * @param non-negative-int|null $scale
+         */
         public function add(Number|string|int $num, ?int $scale = null): Number {}
 
+        /**
+         * @param Number|numeric-string|int $num
+         * @param non-negative-int|null $scale
+         */
         public function sub(Number|string|int $num, ?int $scale = null): Number {}
 
+        /**
+         * @param Number|numeric-string|int $num
+         * @param non-negative-int|null $scale
+         */
         public function mul(Number|string|int $num, ?int $scale = null): Number {}
 
+        /**
+         * @param Number|numeric-string|int $num
+         * @param non-negative-int|null $scale
+         */
         public function div(Number|string|int $num, ?int $scale = null): Number {}
 
+        /**
+         * @param Number|numeric-string|int $num
+         * @param non-negative-int|null $scale
+         */
         public function mod(Number|string|int $num, ?int $scale = null): Number {}
 
         /**
-         * @return list<Number>
+         * @param Number|numeric-string|int $num
+         * @param non-negative-int|null $scale
+         * @return array{0: Number, 1: Number}
          */
         public function divmod(Number|string|int $num, ?int $scale = null): array {}
 
+        /**
+         * @param Number|numeric-string|int $exponent
+         * @param Number|numeric-string|int $modulus
+         * @param non-negative-int|null $scale
+         */
         public function powmod(Number|string|int $exponent, Number|string|int $modulus, ?int $scale = null): Number {}
 
+        /**
+         * @param Number|numeric-string|int $exponent
+         * @param non-negative-int|null $scale
+         */
         public function pow(Number|string|int $exponent, ?int $scale = null): Number {}
 
+        /**
+         * @param non-negative-int|null $scale
+         */
         public function sqrt(?int $scale = null): Number {}
 
         public function floor(): Number {}
@@ -100,8 +191,13 @@ namespace BcMath {
 
         public function round(int $precision = 0, \RoundingMode $mode = \RoundingMode::HalfAwayFromZero): Number {}
 
+        /**
+         * @param Number|numeric-string|int $num
+         * @param non-negative-int|null $scale
+         */
         public function compare(Number|string|int $num, ?int $scale = null): int {}
 
+        /** @return numeric-string */
         public function __toString(): string {}
 
         public function __serialize(): array {}
