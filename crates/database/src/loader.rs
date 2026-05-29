a64fbf9c8d3d131c38b57ce62ed9874cfc9ca6f5
azjezz <azjezz@protonmail.com>
2026-05-29 21:20:45 +0100

    chore(database): ignore clippy issue
    Signed-off-by: azjezz <azjezz@protonmail.com>

--- crates/database/src/loader.rs
+++ crates/database/src/loader.rs
@@ -504,7 +504,7 @@ pub(crate) fn resolve_file_type((host_specificity, vendored_specificity): (Optio
 }
 
 #[cfg(test)]
-#[allow(clippy::unwrap_used)]
+#[allow(clippy::unwrap_used, clippy::expect_used)]
 mod tests {
     use super::*;
     use crate::DatabaseReader;
