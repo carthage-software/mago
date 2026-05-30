35e53c2f292da0cc45821bda7da98f51c9ce50c3
azjezz <azjezz@protonmail.com>
2026-05-30 15:08:32 +0100

    chore: update to rust 1.96.0
    Signed-off-by: azjezz <azjezz@protonmail.com>

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
       let
         pkgs = import nixpkgs { inherit system; };
         isDarwin = pkgs.stdenv.isDarwin;
-        toolchain = "1.95.0";
+        toolchain = "1.96.0";
         php = pkgs.php84;
         composer = pkgs.php84Packages.composer;
       in
